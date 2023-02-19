85194940ff92eecafebda5980450b217425f6573
Valmir Ademar Weide Knäsel Júnior <valmirknasel@gmail.com>
2023-02-19 00:23:08 -0400

    Commit inicial

---
+++ src/main/java/com/knasel/billstopay/BillsToPayApplication.java
@@ -0,0 +1,13 @@
+package com.knasel.billstopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillsToPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillsToPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/knasel/billstopay/BillsToPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knasel.billstopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillsToPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
